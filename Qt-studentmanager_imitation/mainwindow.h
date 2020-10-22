01e0854f9e6f1b98aba061a1e06bd17ef000e707
lz <1043855780@qq.com>
2020-10-22 19:24:02 +0800

    使用 sql 模块，增加按钮点击相应（槽函数），连接查询数据库。

--- Qt-studentmanager_imitation/mainwindow.h
+++ Qt-studentmanager_imitation/mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QRadioButton>
+//#include "studentform.h"
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QMessageBox>
+//#include<manger.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +22,17 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_LoginButton_clicked();
+    void show2();
+   // void open();
+    void on_remitButton_clicked();
+
+signals:
+    void sendData(QString);
 private:
     Ui::MainWindow *ui;
+//    studentForm *f;
+//    manger *m;
 };
 #endif // MAINWINDOW_H
