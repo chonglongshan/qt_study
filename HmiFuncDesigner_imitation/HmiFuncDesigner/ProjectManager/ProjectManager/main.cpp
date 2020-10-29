aad3bc033cc0ee731d9ceeb8bfd8f8d45509da21
lz <1043855780@qq.com>
2020-10-29 15:56:25 +0800

    原 HmiFuncDesigner 项目 main 文件中使用的 QtSingleApplication 、 QTextCodec 、 QFont 、 PluginManager 都暂不仿写。

--- HmiFuncDesigner_imitation/HmiFuncDesigner/ProjectManager/ProjectManager/main.cpp
+++ HmiFuncDesigner_imitation/HmiFuncDesigner/ProjectManager/ProjectManager/main.cpp
@@ -5,6 +5,15 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    // 原项目使用了 Qt Extended 的 QtSingleApplication ，实现程序只运行一个实例。
+    // 原项目使用了 Qt Extended 的 QtSingleApplication ，设置公司和软件版本信息等。
+
+    // 原项目使用 QTextCodec 进行编码设置。
+
+    // 原项目使用 QFont 进行字体设置。
+
+    // 原项目使用 PluginManager 支持插件 。
+
     MainWindow w;
     w.show();
     return a.exec();
