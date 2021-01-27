29f2f8ca1ff9134785299061755a92e90aa45aad
lz <1043855780@qq.com>
2021-01-27 09:46:28 +0800

    移植 blockingmaster 的 内容

--- blockingmaster_study/blockingmaster_study.pro
+++ blockingmaster_study/blockingmaster_study.pro
@@ -16,14 +16,13 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    dialog.cpp \
     main.cpp \
-    mainwindow.cpp
+    masterthread.cpp
 
 HEADERS += \
-    mainwindow.h
-
-FORMS += \
-    mainwindow.ui
+    dialog.h \
+    masterthread.h
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
--- blockingmaster_study/masterthread.h
+++ blockingmaster_study/masterthread.h
@@ -48,14 +48,39 @@
 **
 ****************************************************************************/
 
-#include "dialog.h"
+#ifndef MASTERTHREAD_H
+#define MASTERTHREAD_H
 
-#include <QApplication>
+#include <QMutex>
+#include <QThread>
+#include <QWaitCondition>
 
-int main(int argc, char *argv[])
+//! [0]
+class MasterThread : public QThread
 {
-    QApplication app(argc, argv);
-    Dialog dialog;
-    dialog.show();
-    return app.exec();
-}
+    Q_OBJECT
+
+public:
+    explicit MasterThread(QObject *parent = nullptr);
+    ~MasterThread();
+
+    void transaction(const QString &portName, int waitTimeout, const QString &request);
+
+signals:
+    void response(const QString &s);
+    void error(const QString &s);
+    void timeout(const QString &s);
+
+private:
+    void run() override;
+
+    QString m_portName;
+    QString m_request;
+    int m_waitTimeout = 0;
+    QMutex m_mutex;
+    QWaitCondition m_cond;
+    bool m_quit = false;
+};
+//! [0]
+
+#endif // MASTERTHREAD_H
