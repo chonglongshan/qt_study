9ea3ea9a5ee537ba143283d6c9d3855c7de46a40
lz <1043855780@qq.com>
2021-03-29 16:33:10 +0800

    实现菜单及基本菜单项的创建。

--- qt_subdirs/temp/test_Plug_and_Paint_Example/mainwindow.cpp
+++ qt_subdirs/temp/test_Plug_and_Paint_Example/mainwindow.cpp
@@ -1,11 +1,93 @@
 #include "mainwindow.h"
 
+#include <QtWidgets>
+
+#define INCOMPLETE_FUNCTION qDebug() << tr("incomplete function: %1").arg(QT_MESSAGELOG_FUNC);
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    createActions();
+    createMenus();
 }
 
 MainWindow::~MainWindow()
 {
 }
+
+void MainWindow::createActions()
+{
+    openAct = new QAction(tr("&Open..."), this);
+    openAct->setShortcuts(QKeySequence::Open);
+    connect(openAct, &QAction::triggered, this, &MainWindow::open);
+
+    saveAsAct = new QAction(tr("&Save As..."), this);
+    saveAsAct->setShortcuts(QKeySequence::SaveAs);
+    connect(saveAsAct, &QAction::triggered, this, &MainWindow::saveAs);
+
+    exitAct = new QAction(tr("E&xit"), this);
+    exitAct->setShortcuts(QKeySequence::Quit);
+    connect(exitAct, &QAction::triggered, this, &MainWindow::close);
+
+    brushColorAct = new QAction(tr("&Brush Color..."), this);
+    connect(brushColorAct, &QAction::triggered, this, &MainWindow::brushColor);
+
+    brushWidthAct = new QAction(tr("&Brush Width..."), this);
+    connect(brushWidthAct, &QAction::triggered, this, &MainWindow::brushWidth);
+
+    brushActionGroup = new QActionGroup(this);
+
+    aboutAct = new QAction(tr("&About"), this);
+    connect(aboutAct, &QAction::triggered, this, &MainWindow::about);
+
+    aboutQtAct = new QAction(tr("About &Qt"), this);
+    connect(aboutQtAct, &QAction::triggered, qApp, &QApplication::aboutQt);
+
+    aboutPluginsAct = new QAction(tr("About &Plugins"), this);
+    connect(aboutPluginsAct, &QAction::triggered, this, &MainWindow::aboutPlugins);
+}
+
+void MainWindow::createMenus()
+{
+    fileMenu = menuBar()->addMenu(tr("&File"));
+    fileMenu->addAction(openAct);
+    fileMenu->addAction(saveAsAct);
+    fileMenu->addSeparator();
+    fileMenu->addAction(exitAct);
+
+    brushMenu = menuBar()->addMenu(tr("&Brush"));
+    brushMenu->addAction(brushColorAct);
+    brushMenu->addAction(brushWidthAct);
+    brushMenu->addSeparator();
+
+    shapesMenu = menuBar()->addMenu(tr("&Shapes"));
+
+    filterMenu = menuBar()->addMenu(tr("&Filter"));
+
+    menuBar()->addSeparator();
+
+    helpMenu = menuBar()->addMenu(tr("&Help"));
+    helpMenu->addAction(aboutAct);
+    helpMenu->addAction(aboutQtAct);
+    helpMenu->addAction(aboutPluginsAct);
+}
+
+void MainWindow::open()
+{INCOMPLETE_FUNCTION}
+
+bool MainWindow::saveAs()
+{INCOMPLETE_FUNCTION return false;}
+
+void MainWindow::brushColor()
+{INCOMPLETE_FUNCTION}
+
+void MainWindow::brushWidth()
+{INCOMPLETE_FUNCTION}
+
+void MainWindow::about()
+{INCOMPLETE_FUNCTION}
+
+void MainWindow::aboutPlugins()
+{INCOMPLETE_FUNCTION}
+
 
--- qt_subdirs/temp/test_Plug_and_Paint_Example/mainwindow.h
+++ qt_subdirs/temp/test_Plug_and_Paint_Example/mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+class QActionGroup;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -10,5 +12,35 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private:
+    void createActions();
+    void createMenus();
+
+private:
+    QAction *openAct = nullptr;
+    QAction *saveAsAct = nullptr;
+    QAction *exitAct = nullptr;
+    QAction *brushWidthAct = nullptr;
+    QAction *brushColorAct = nullptr;
+    QAction *aboutAct = nullptr;
+    QAction *aboutQtAct = nullptr;
+    QAction *aboutPluginsAct = nullptr;
+    QActionGroup *brushActionGroup = nullptr;
+
+private slots:
+    void open();
+    bool saveAs();
+    void brushColor();
+    void brushWidth();
+    void about();
+    void aboutPlugins();
+
+private:
+    QMenu *fileMenu = nullptr;
+    QMenu *brushMenu = nullptr;
+    QMenu *shapesMenu = nullptr;
+    QMenu *filterMenu = nullptr;
+    QMenu *helpMenu = nullptr;
 };
 #endif // MAINWINDOW_H
