2aa504341d46f140082312456f0ba5ae8a6d76a0
lz <1043855780@qq.com>
2021-04-02 11:19:16 +0800

    实现主程序界面框架。

--- qt_subdirs/temp/test_Style_Plugin_Example/main.cpp
+++ qt_subdirs/temp/test_Style_Plugin_Example/main.cpp
@@ -2,10 +2,16 @@
 
 #include <QApplication>
 
+#include <QtWidgets>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QApplication::setStyle(QStyleFactory::create("simplestyle"));
+
     MainWindow w;
+    w.resize(200, 50);
     w.show();
     return a.exec();
 }
--- qt_subdirs/temp/test_Style_Plugin_Example/mainwindow.cpp
+++ qt_subdirs/temp/test_Style_Plugin_Example/mainwindow.cpp
@@ -1,8 +1,23 @@
 #include "mainwindow.h"
 
+#include <QtWidgets>
+
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+    : QWidget(parent)
 {
+    QPushButton *styledButton = new QPushButton(tr("Big Red Button"));
+
+    QGridLayout *layout = new QGridLayout;
+    layout->addWidget(styledButton);
+
+    QGroupBox *styleBox = new QGroupBox(tr("A simple style button"));
+    styleBox->setLayout(layout);
+
+    QGridLayout *outerLayout = new QGridLayout;
+    outerLayout->addWidget(styleBox, 0, 0);
+    setLayout(outerLayout);
+
+    setWindowTitle(tr("Style Plugin Example"));
 }
 
 MainWindow::~MainWindow()
