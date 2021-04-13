e5b9736067f47b6c06f47ac015d3ed62b0a51af9
lz <1043855780@qq.com>
2021-04-13 21:04:00 +0800

    调测串口助手的数据收发方法。

--- qt_subdirs/temp/test_QSerialPort/mainwindow.cpp
+++ qt_subdirs/temp/test_QSerialPort/mainwindow.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "lzchuankouzhushou.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //
+    LzChuanKouXinXi chuan_kou;
+    LzChuanKouZhuShou zhu_shou;
+    zhu_shou.lzFaSongBingJieShouShuJu(chuan_kou, QByteArray());
 }
 
 MainWindow::~MainWindow()
