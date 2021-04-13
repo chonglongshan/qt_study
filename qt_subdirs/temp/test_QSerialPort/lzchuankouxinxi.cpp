b4028bfce7464603b0a9c948522701eb1e0a7c33
lz <1043855780@qq.com>
2021-04-13 21:01:37 +0800

    串口信息类实现封装主要的串口信息。并实现等于、不等于等操作符函数，以及赋值操作符函数。以及转换位字符串（调试信息）的方法。成员数据提供默认值。

--- qt_subdirs/temp/test_QSerialPort/lzchuankouxinxi.cpp
+++ qt_subdirs/temp/test_QSerialPort/lzchuankouxinxi.cpp
@@ -1,6 +1,56 @@
 #include "lzchuankouxinxi.h"
 
+#include <QTextStream>
+
 LzChuanKouXinXi::LzChuanKouXinXi(QObject *parent) : QObject(parent)
+  , _lz_chuan_kou_hao("COM1")
+  , _lz_bo_te_lv(9600)
+  , _lz_jiao_yan_fang_shi('N')
+  , _lz_shu_ju_wei(8)
+  , _lz_ting_zhi_wei(1)
 {
 
+}
+
+bool operator==(const LzChuanKouXinXi &xx1, const LzChuanKouXinXi &xx2)
+{
+    if((xx1._lz_chuan_kou_hao == xx2._lz_chuan_kou_hao)
+            && (xx1._lz_bo_te_lv == xx2._lz_bo_te_lv)
+            && (xx1._lz_jiao_yan_fang_shi == xx2._lz_jiao_yan_fang_shi)
+            && (xx1._lz_shu_ju_wei == xx2._lz_shu_ju_wei)
+            && (xx1._lz_ting_zhi_wei == xx2._lz_ting_zhi_wei))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool operator!=(const LzChuanKouXinXi &xx1, const LzChuanKouXinXi &xx2)
+{
+    return !(xx1 == xx2);
+}
+
+//QDataStream &operator<<(QDataStream &s, const LzChuanKouXinXi &x)
+//{
+//    s << x._lz_chuan_kou_hao
+//      << x._lz_bo_te_lv
+//      << x._lz_jiao_yan_fang_shi
+//      << x._lz_shu_ju_wei
+//      << x._lz_ting_zhi_wei;
+//    return s;
+//}
+
+QString LzChuanKouXinXi::lzToString() const
+{
+    QString s;
+    QTextStream ts(&s);
+    ts << _lz_chuan_kou_hao << ", "
+       << _lz_bo_te_lv << ", "
+       << _lz_jiao_yan_fang_shi << ", "
+       << _lz_shu_ju_wei << ", "
+       << _lz_ting_zhi_wei;
+    return (*(ts.string()));
 }
--- qt_subdirs/temp/test_QSerialPort/lzchuankouxinxi.h
+++ qt_subdirs/temp/test_QSerialPort/lzchuankouxinxi.h
@@ -9,9 +9,34 @@ class LzChuanKouXinXi : public QObject
 public:
     explicit LzChuanKouXinXi(QObject *parent = nullptr);
 
-signals:
+    //
+    LzChuanKouXinXi &operator=(const LzChuanKouXinXi &other)
+    {
+        _lz_chuan_kou_hao = other._lz_chuan_kou_hao;
+        _lz_bo_te_lv = other._lz_bo_te_lv;
+        _lz_jiao_yan_fang_shi = other._lz_jiao_yan_fang_shi;
+        _lz_shu_ju_wei = other._lz_shu_ju_wei;
+        _lz_ting_zhi_wei = other._lz_ting_zhi_wei;
 
-public slots:
+        return *this;
+    }
+
+    //
+    QString lzToString() const;
+
+private:
+    QString _lz_chuan_kou_hao;
+    int _lz_bo_te_lv;
+    char _lz_jiao_yan_fang_shi;
+    int _lz_shu_ju_wei;
+    int _lz_ting_zhi_wei;
+
+//    friend class LzChuanKouZhuShou;
+    friend bool operator==(const LzChuanKouXinXi &xx1, const LzChuanKouXinXi &xx2);
+    friend bool operator!=(const LzChuanKouXinXi &xx1, const LzChuanKouXinXi &xx2);
+//    friend QDataStream &operator<<(QDataStream &s, const LzChuanKouXinXi &x);
 };
+
+//QDataStream &operator<<(QDataStream &s, const LzChuanKouXinXi &x);
 
 #endif // LZCHUANKOUXINXI_H
