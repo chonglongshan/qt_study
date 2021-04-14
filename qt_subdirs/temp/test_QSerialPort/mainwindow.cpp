ac0a0bc0fde6b4311bba1b35ad200cdf31e8343c
lz <1043855780@qq.com>
2021-04-14 13:37:07 +0800

    指定请求数据进行调测收发数据。

--- qt_subdirs/temp/test_QSerialPort/mainwindow.cpp
+++ qt_subdirs/temp/test_QSerialPort/mainwindow.cpp
@@ -2,6 +2,8 @@
 #include "ui_mainwindow.h"
 #include "lzchuankouzhushou.h"
 
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -11,7 +13,12 @@ MainWindow::MainWindow(QWidget *parent)
     //
     LzChuanKouXinXi chuan_kou("COM3", 115200, 'N', 8, 1);
     LzChuanKouZhuShou zhu_shou;
-    zhu_shou.lzFaSongBingJieShouShuJu(chuan_kou, QByteArray());
+
+    char request_data_array[] = {static_cast<char>(0xAA), 0x55, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    QByteArray request(request_data_array, sizeof (request_data_array)/sizeof(char));
+    QByteArray response = zhu_shou.lzFaSongBingJieShouShuJu(chuan_kou, request);
+    qDebug() << request;
+    qDebug() << response;
 }
 
 MainWindow::~MainWindow()
